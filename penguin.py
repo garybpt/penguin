6a3b3bb371b87ec3ad96c9adb37ab41db038d7cd
Gary Butterfield <gary.butterfield@joyjunction.co.uk>
2024-01-20 12:25:22 +0000

    Minor updates.

--- penguin.py
+++ penguin.py
@@ -4,47 +4,35 @@ yes = 'yes'
 no = 'no'
 
 print("Is this a penguin? Lert's find out...")
-
 print("Is it black and white?")
 answer = input()
-if answer == yes:
 
+if answer == yes:
     print("is it one of 101?")
     answer = input()
     if answer == yes:
-
         print("It's a dalmation")
-    
     else:
         print("Can it fly?")
         answer = input()
         if answer == yes:
-
             print("Is it bad luck?")
             answer = input()
             if answer == yes:
-
                 print("It's a magpie.")
-            
             else:
-                print("It's a zebra on a plane.")
-        
+                print("It's a zebra on a plane.")   
         else:
-            print("Does it waddles?")
+            print("Does it waddle?")
             answer = input()
             if answer == yes:
-
                 print("Is it wearing a nappy?")
                 answer = input()
                 if answer == yes:
-
                     print("It's a baby wearing a tux!")
-
                 else:
                     print("IT'S A PENGUIN!")
-
             else:
                 print("It's a newspaper.")
-
 else:
     print("It is not a penguin.")
